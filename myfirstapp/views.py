7ffde7b31a7a071693dabe0cb8c216d68a0c4b21
kunal <kunalkumar.kus@gmail.com>
2019-07-07 18:16:48 +0530

    update

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import blog
 
-# Create your views here.
+def index(request):
+	blog_var = blog.objects.all()
+	return render(request,'myfirstapp/index.html',{'blog_kun':blog_var})
